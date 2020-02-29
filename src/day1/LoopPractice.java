dc8d180e740cc0f8edbee7439bcfda1eef283090
okandogan <okan2317@gmail.com>
2020-02-29 15:46:38 -0500

    LoopPractice Added

---
+++ src/day1/LoopPractice.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class LoopPractice {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <=10 ; i++) {
+            System.out.print(i);
+            if(i<=9){
+            System.out.print(", ");
+            }
+        }
+
+    }
+}
